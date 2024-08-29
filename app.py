54e0344e1a711c2c8cc4a6b37dcc49ac787b1c5f
Chetan <chetan19396@gmail.com>
2024-08-29 23:49:08 +0530

    update app.py

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ App = st.text_input("Enter the google play store app URL to scrape the reviews:"
 if st.button("Submit"):
     st.write(f"The {App} submitted")
 
+
+# Sidebar content
+st.sidebar.title("Leveraging User Insights for Product Innovation")
+
+st.sidebar.write(
+    "User reviews offer valuable insights into what works well and what doesn’t. By carefully examining this feedback, product managers can uncover key strengths and weaknesses of their products, allowing them to enhance successful features and address any pain points to improve overall user satisfaction"
+)
+
