7016bc2bea04737292b6c964d25fa0b60f04dcef
Chetan <chetan19396@gmail.com>
2024-08-30 10:20:22 +0530

    appid

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ st.title("App Reviews Research: Understanding User Feedback and Sentiment")
 # Text input
 App = st.text_input("Enter the google play store app URL to scrape the reviews:")
 
-app_id=extract_app_id(App)
+# app_id=extract_app_id(App)
 
 if st.button("Fetch Reviews"):
  if app_id:
+  app_id=extract_app_id(App)
   st.write(f"Fetching reviews for {app_id}...")
             
   df = get_app_reviews_dataframe(
